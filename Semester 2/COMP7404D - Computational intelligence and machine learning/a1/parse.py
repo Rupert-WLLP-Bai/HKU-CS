ab69057be710fd1d84d7d255359b6ad579e4df1f
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2025-02-25 21:15:45 +0800

    parser

--- Semester 2/COMP7404D - Computational intelligence and machine learning/a1/parse.py
+++ Semester 2/COMP7404D - Computational intelligence and machine learning/a1/parse.py
@@ -1,7 +1,36 @@
 import os, sys
 def read_graph_search_problem(file_path):
     #Your p1 code here
-    problem = ''
+    """ a sample problem file
+    start_state: A
+    goal_states: G
+    A 0
+    B 0
+    C 0
+    G 0
+    A B 1.0
+    B A 2.0
+    B C 4.0
+    C A 8.0
+    C G 16.0
+    C B 32.0
+    """
+    # read the file
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    # parse the file
+    start_state = lines[0].split()[1]
+    goal_states = lines[1].split()[1]
+    graph = {}
+    for line in lines[2:]:
+        if len(line.split()) == 2:
+            state = line.split()[0]
+            graph[state] = {}
+        else:
+            state1, state2, cost = line.split()
+            graph[state1][state2] = float(cost)
+    # return the problem
+    problem = {'start_state': start_state, 'goal_states': goal_states, 'graph': graph}
     return problem
 
 def read_8queens_search_problem(file_path):
