a7a019eb3341a5e73b99871ab4a46cdb02318540
Rupert-WLLP-Bai <cwllp1230@gmail.com>
2025-02-26 18:16:19 +0800

    comp 7404 a1 p1

--- Semester 2/COMP7404D - Computational intelligence and machine learning/a1/p1.py
+++ Semester 2/COMP7404D - Computational intelligence and machine learning/a1/p1.py
@@ -1,9 +1,56 @@
 import sys, grader, parse
 
+from collections import deque
+
 def dfs_search(problem):
-    #Your p1 code here
-    solution = 'Ar D C\nAr C G'
-    return solution
+    start_state = problem['start_state']
+    goal_states = problem['goal_states']
+    graph = problem['graph']
+    
+    # DFS-GSA
+    # Returns: f'{" ".join(explored)}\n{" ".join(path)}'
+    
+    # Initialize explored list, frontier stack, and path list
+    explored = []
+    frontier = deque([start_state])  # Use deque as stack
+    path = []
+    parent = {start_state: None}
+    goal_state = None
+
+    while frontier:
+        state = frontier.pop()  # Pop from the stack (LIFO)
+        
+        if state not in explored:
+            explored.append(state)
+            
+            # Check if we have found a goal state
+            if state in goal_states:
+                goal_state = state
+                break
+
+            # Add neighbors to the frontier (stack) for exploration
+            for neighbor in graph[state]:
+                if neighbor not in explored:
+                    frontier.append(neighbor)
+                    parent[neighbor] = state
+    
+    # If a goal state is found, reconstruct the path
+    if goal_state:
+        path.append(goal_state)
+        while parent[goal_state] is not None:
+            goal_state = parent[goal_state]
+            path.append(goal_state)
+        
+        path.reverse()  # Reverse path to get the correct order from start to goal
+        
+        # remove the goal state from explored list
+        explored.remove(path[-1])
+    else:
+        path = []  # No path if no goal state is found
+    
+    return f'{" ".join(explored)}\n{" ".join(path)}'
+
+
 
 if __name__ == "__main__":
     test_case_id = int(sys.argv[1])
