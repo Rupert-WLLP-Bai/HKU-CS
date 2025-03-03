2410d3b082cdfc1976f23d53c988ba96c48b7e5c
Rupert-WLLP-Bai <cwllp1230@gmail.com>
2025-03-04 00:00:50 +0800

    comp 7404d a2 p1

--- Semester 2/COMP7404D - Computational intelligence and machine learning/a2/p1.py
+++ Semester 2/COMP7404D - Computational intelligence and machine learning/a2/p1.py
@@ -13,13 +13,13 @@ def random_play_single_ghost(problem):
     seed = problem['seed']
     random.seed(seed, version=1)
     layout = problem['layout']
-    pacman_position, ghost_position, food_positions, board_size = extract_board_state(layout)
+    pacman_position, ghost_position, food_positions, wall_positions, board_size = extract_board_state(layout)
     score = 0
     step = 0
     result_str = ''
     result_str += 'seed: {}\n'.format(seed)
     result_str += '0\n'
-    result_str += reconstruct_board_state(pacman_position, ghost_position, food_positions, board_size)
+    result_str += reconstruct_board_state(pacman_position, ghost_position, food_positions, wall_positions, board_size)
     
     # 1. pacman eats all the food and wins
     # 2. pacman is eaten by the ghost
@@ -29,14 +29,13 @@ def random_play_single_ghost(problem):
         # choose a random direction which is not blocked by a wall
         available_directions = []
         for direction in DIRECTIONS:
-            i, j = move(pacman_position, direction, board_size)
+            i, j = move(pacman_position, direction)
             if layout[i][j] != '%':
                 available_directions.append(direction)
         direction = random.choice(available_directions)
+        # print('step:{}, available_directions:{}, direction:{}'.format(step, available_directions, direction))
         p_moving_str = f'{step}: P moving {direction}'
-        pacman_position = move(pacman_position, direction, board_size)
-        board_str = reconstruct_board_state(pacman_position, ghost_position, food_positions, board_size)
-        result_str += p_moving_str + '\n' + board_str
+        pacman_position = move(pacman_position, direction)
         # print('pacman_move:', direction)
         score += PACMAN_MOVING_SCORE
         # pacman eats food
@@ -45,30 +44,46 @@ def random_play_single_ghost(problem):
             score += EAT_FOOD_SCORE
             if not food_positions:
                 score += PACMAN_WIN_SCORE
+                board_str = reconstruct_board_state(pacman_position, ghost_position, food_positions, wall_positions, board_size)
+                result_str += p_moving_str + '\n' + board_str
                 result_str += f'score: {score}\nWIN: Pacman'
                 break
+        board_str = reconstruct_board_state(pacman_position, ghost_position, food_positions, wall_positions, board_size)
+        result_str += p_moving_str + '\n' + board_str
         # pacman is eaten by the ghost
         if pacman_position == ghost_position:
             score += PACMAN_EATEN_SCORE
             result_str += f'score: {score}\nWIN: Ghost'
             break
+        else:
+            # pacman is not eaten by the ghost
+            result_str += f'score: {score}\n'
         # ghost moves
         step += 1
         available_directions = []
         for direction in DIRECTIONS:
-            i, j = move(ghost_position, direction, board_size)
+            i, j = move(ghost_position, direction)
             if layout[i][j] != '%':
                 available_directions.append(direction)
         direction = random.choice(available_directions)
+        # print('step:{}, available_directions:{}, direction:{}'.format(step, available_directions, direction))
         w_moving_str = f'{step}: W moving {direction}'
-        ghost_position = move(ghost_position, direction, board_size)
-        board_str = reconstruct_board_state(pacman_position, ghost_position, food_positions, board_size)
+        ghost_position = move(ghost_position, direction)
+        board_str = reconstruct_board_state(pacman_position, ghost_position, food_positions, wall_positions, board_size)
         result_str += w_moving_str + '\n' + board_str
+        # pacman is eaten by the ghost
+        if pacman_position == ghost_position:
+            score += PACMAN_EATEN_SCORE
+            result_str += f'score: {score}\nWIN: Ghost'
+            break
+        else:
+            # pacman is not eaten by the ghost
+            result_str += f'score: {score}\n'
         # print('ghost_move:', direction)
         
     return result_str
 
-def move(position, direction, board_size):
+def move(position, direction):
     i, j = position
     if direction == 'E':
         j += 1
@@ -86,11 +101,13 @@ def extract_board_state(layout):
     1. pacman position
     2. ghost position
     3. food positions
-    4. board size
+    4. wall positions
+    5. board size
     '''
     pacman_position = None
     ghost_position = None
     food_positions = []
+    wall_positions = []
     board_size = (len(layout), len(layout[0]))
     for i in range(len(layout)):
         for j in range(len(layout[i])):
@@ -100,9 +117,11 @@ def extract_board_state(layout):
                 ghost_position = (i, j)
             elif layout[i][j] == '.':
                 food_positions.append((i, j))
-    return pacman_position, ghost_position, food_positions, board_size
+            elif layout[i][j] == '%':
+                wall_positions.append((i, j))
+    return pacman_position, ghost_position, food_positions, wall_positions, board_size
 
-def reconstruct_board_state(pacman_position, ghost_position, food_positions, board_size):
+def reconstruct_board_state(pacman_position, ghost_position, food_positions, wall_positions, board_size):
     """
     '%': Wall
     'W': Ghost
@@ -113,10 +132,8 @@ def reconstruct_board_state(pacman_position, ghost_position, food_positions, boa
     # initialize the board
     layout = [[' ' for _ in range(board_size[1])] for _ in range(board_size[0])]
     # wall
-    for i in range(board_size[0]):
-        layout[i][0] = layout[i][-1] = '%'
-    for j in range(board_size[1]):
-        layout[0][j] = layout[-1][j] = '%'
+    for i, j in wall_positions:
+        layout[i][j] = '%'
     # pacman
     i, j = pacman_position
     layout[i][j] = 'P'
@@ -126,6 +143,10 @@ def reconstruct_board_state(pacman_position, ghost_position, food_positions, boa
     # food
     for i, j in food_positions:
         layout[i][j] = '.'
+    # if ghost is on the food, change the food to ghost
+    if ghost_position in food_positions:
+        i, j = ghost_position
+        layout[i][j] = 'W'
     # return the layout to string format
     layout_str = ''
     for row in layout:
